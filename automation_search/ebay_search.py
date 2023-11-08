e48f80f87bf3d79cb2ad4b175802c95c7dc05b5a
natautomation <147779652+natautomation@users.noreply.github.com>
2023-11-07 21:53:28 -0800

    Create ebay_search.py

---
+++ automation_search/ebay_search.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+
+
+
+
+
+
+driver.quit()
