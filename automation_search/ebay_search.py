9a0df342053847848500d865b036cadb78c332f4
natautomation <147779652+natautomation@users.noreply.github.com>
2023-11-08 20:15:03 -0800

    Update ebay_search.py

--- automation_search/ebay_search.py
+++ automation_search/ebay_search.py
@@ -3,12 +3,12 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+#first test for check installation 
 
 driver = webdriver.Chrome()
-
-
-
-
-
+driver.get("http://www.ebay.com")
+wait = WebDriverWait(driver, 5)
+print(driver.current_url)
+driver.close()
 
 driver.quit()
