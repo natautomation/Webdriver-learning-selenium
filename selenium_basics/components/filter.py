0d2ec5e57676fd52f0d7e5f5d7bbdc6d5a902a25
natautomation <147779652+natautomation@users.noreply.github.com>
2023-11-13 16:15:15 -0800

    Update filter.py

--- selenium_basics/components/filter.py
+++ selenium_basics/components/filter.py
@@ -1,1 +1,18 @@
+from selenium_basics.components.base import Base
 
+class LeftFilter(Base):
+    LOCATOR = "//"
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def select_option(self, option, visible=False):
+        print(Base.BASE_VAR)
+        print(LeftFilter.LOCATOR)
+        print(option)
+        print(visible)
+
+
+#--output--
+#/usr/local/bin/python3.9 /Users/g/PycharmProjects/Webdriver-learning-selenium/selenium_basics/components/filter.py
+#Process finished with exit code 0
