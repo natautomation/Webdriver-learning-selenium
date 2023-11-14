e4f8ece89ccb03191747c4a86b98ec7095b0211b
natautomation <147779652+natautomation@users.noreply.github.com>
2023-11-13 16:14:01 -0800

    Update base.py

--- selenium_basics/components/base.py
+++ selenium_basics/components/base.py
@@ -1,1 +1,15 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
+class Base:
+    BASE_VAR = "Base Var"
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click(self, locator):
+        WebDriverWait(self.driver, 10).until(ec.element_to_be_clickable(locator)).click()
+
+
+#--output--
+#/usr/local/bin/python3.9 /Users/gaughan/PycharmProjects/Webdriver-learning-selenium/selenium_basics/components/base.py
+#Process finished with exit code 0
