eab4e483ac230fbd24687b97ce93d286ab34f7b0
natautomation <147779652+natautomation@users.noreply.github.com>
2023-11-15 19:35:16 -0800

    Create filter.py

---
+++ automation_search_15sprint/components/filter.py
@@ -0,0 +1,36 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class LeftFilterPane:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def apply_filter(self, filter_name):
+        filter_xpath = f"//span[contains(text(), '{filter_name}')]"
+        filter_element = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, filter_xpath))
+        )
+        filter_element.click()
+
+    def select_rolex(self):
+        rolex_xpath = '//*[@id="x-refine__group_1__2"]/ul/li[1]/div/a/div/span/input'
+        rolex_checkbox = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, rolex_xpath))
+        )
+        rolex_checkbox.click()
+
+    def uncheck_rolex(self):
+        rolex_xpath = '//li[@class="x-refine__main__list "][contains(., "Brand")]//div[@class="x-refine__multi-select"][contains(., "Rolex")]//a'
+        rolex_checkbox = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, rolex_xpath))
+        )
+        if rolex_checkbox.is_selected():
+            rolex_checkbox.click()
+
+    def check_casio(self):
+        casio_xpath = '//li[@class="x-refine__main__list "][contains(., "Brand")]//div[@class="x-refine__multi-select"][contains(., "Casio")]//a'
+        casio_checkbox = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, casio_xpath))
+        )
+        if not casio_checkbox.is_selected():
+            casio_checkbox.click()
